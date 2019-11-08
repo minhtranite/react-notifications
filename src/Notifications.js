ce3913cf3f615af7861198324bdb69567a4ad72d
edumoreira1506 <contato@eduardoem.com.br>
2019-11-08 16:24:01 -0300

    Refactor <Notifications /> with hooks

--- src/Notifications.js
+++ src/Notifications.js
@@ -4,59 +4,51 @@ import { CSSTransitionGroup } from 'react-transition-group';
 import classnames from 'classnames';
 import Notification from './Notification';
 
-class Notifications extends React.Component {
-  static propTypes = {
-    notifications: PropTypes.array.isRequired,
-    onRequestHide: PropTypes.func,
-    enterTimeout: PropTypes.number,
-    leaveTimeout: PropTypes.number
+const Notifications = ({
+  notifications = [],
+  onRequestHide = () => {  },
+  enterTimeout = 400,
+  leaveTimeout = 400
+}) => {
+  const handleRequestHide = notification => () => {
+    if (onRequestHide) onRequestHide(notification);
   };
 
-  static defaultProps = {
-    notifications: [],
-    onRequestHide: () => {
-    },
-    enterTimeout: 400,
-    leaveTimeout: 400
-  };
+  const className = classnames('notification-container', {
+    'notification-container-empty': notifications.length === 0
+  });
 
-  handleRequestHide = notification => () => {
-    const { onRequestHide } = this.props;
-    if (onRequestHide) {
-      onRequestHide(notification);
-    }
-  };
-
-  render() {
-    const { notifications, enterTimeout, leaveTimeout } = this.props;
-    const className = classnames('notification-container', {
-      'notification-container-empty': notifications.length === 0
-    });
-    return (
-      <div className={className}>
-        <CSSTransitionGroup
-          transitionName="notification"
-          transitionEnterTimeout={enterTimeout}
-          transitionLeaveTimeout={leaveTimeout}
-        >
-          {notifications.map((notification) => {
-            const key = notification.id || new Date().getTime();
-            return (
-              <Notification
-                key={key}
-                type={notification.type}
-                title={notification.title}
-                message={notification.message}
-                timeOut={notification.timeOut}
-                onClick={notification.onClick}
-                onRequestHide={this.handleRequestHide(notification)}
-              />
-            );
-          })}
-        </CSSTransitionGroup>
-      </div>
-    );
-  }
+  return (
+    <div className={className}>
+      <CSSTransitionGroup
+        transitionName="notification"
+        transitionEnterTimeout={enterTimeout}
+        transitionLeaveTimeout={leaveTimeout}
+      >
+        {notifications.map((notification) => {
+          const key = notification.id || new Date().getTime();
+          return (
+            <Notification
+              key={key}
+              type={notification.type}
+              title={notification.title}
+              message={notification.message}
+              timeOut={notification.timeOut}
+              onClick={notification.onClick}
+              onRequestHide={handleRequestHide(notification)}
+            />
+          );
+        })}
+      </CSSTransitionGroup>
+    </div>
+  );
 }
+
+Notifications.propTypes = {
+  notifications: PropTypes.array.isRequired,
+  onRequestHide: PropTypes.func,
+  enterTimeout: PropTypes.number,
+  leaveTimeout: PropTypes.number
+};
 
 export default Notifications;
